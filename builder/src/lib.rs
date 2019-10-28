ecca96c97873046b637c54913225c9dd8ab7dca4
Peter Williams <pwil3058@gmail.com>
2019-10-28 10:47:08 +1000

    Pass builder-05 test
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,8 @@
 extern crate proc_macro;
+extern crate proc_macro2;
 
 use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
 use quote::quote;
 use syn::{parse_macro_input, Data, DeriveInput, Fields};
 
@@ -8,7 +10,12 @@ use syn::{parse_macro_input, Data, DeriveInput, Fields};
 pub fn derive(input: TokenStream) -> TokenStream {
     let parsed_input: DeriveInput = parse_macro_input!(input);
     let struct_name = parsed_input.ident;
-    let mut tokens = vec![];
+    let builder_name = Ident::new(&format!("{}{}", struct_name, "Builder"), Span::call_site());
+    let mut declr_tokens = vec![];
+    let mut init_tokens = vec![];
+    let mut fn_tokens = vec![];
+    let mut build_tokens = vec![];
+    let mut list_tokens = vec![];
     if let Data::Struct(s) = parsed_input.data {
         if let Fields::Named(fields) = s.fields {
             println!("num: {}", fields.named.len());
@@ -17,12 +24,34 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 let f_name = field.ident.as_ref().unwrap();
                 let f_type = &field.ty;
                 let token = quote! {
-                    pub fn #f_name<'a>(&'a mut self, #f_name: #f_type) -> &'a mut Self {
-                        self.#f_name = #f_name;
+                    #f_name: Option<#f_type>,
+                };
+                declr_tokens.push(token);
+                let token = quote! {
+                    #f_name: None,
+                };
+                init_tokens.push(token);
+                let token = quote! {
+                    pub fn #f_name(&mut self, #f_name: #f_type) -> &mut Self {
+                        self.#f_name = Some(#f_name);
                         self
                     }
                 };
-                tokens.push(token);
+                fn_tokens.push(token);
+                let msg = format!("'{}' field has not been set", stringify!(#f_name));
+                let token = quote! {
+                    let #f_name;
+                    if let Some(ref val) = self.#f_name {
+                        #f_name = val.clone();
+                    } else {
+                        return Err(#msg.to_string());
+                    };
+                };
+                build_tokens.push(token);
+                let token = quote! {
+                    #f_name,
+                };
+                list_tokens.push(token);
             }
         } else {
             panic!("'Builder' can only be derived for structs with named fields.")
@@ -32,8 +61,27 @@ pub fn derive(input: TokenStream) -> TokenStream {
     }
 
     let tokens = quote! {
+        pub struct #builder_name {
+            #(#declr_tokens)*
+        }
+
         impl #struct_name {
-            #(#tokens)*
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    #(#init_tokens)*
+                }
+            }
+        }
+
+        impl #builder_name {
+            #(#fn_tokens)*
+
+            pub fn build(&mut self) -> Result<#struct_name, String> {
+                #(#build_tokens)*
+                Ok(#struct_name {
+                    #(#list_tokens)*
+                })
+            }
         }
     };
 
