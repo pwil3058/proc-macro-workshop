7b6f927140da3137b45c7c4d8f00122ec8240f31
Peter Williams <pwil3058@gmail.com>
2019-10-29 20:56:38 +1000

    Start implementing debug
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -10,7 +10,13 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let parsed_input: syn::DeriveInput = syn::parse_macro_input!(input);
     let _input_name = parsed_input.ident;
 
-    let tokens = quote! {};
+    let tokens = quote! {
+        impl std::fmt::Debug for Whatever {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "Whatever{{}}")
+            }
+        }
+    };
 
     proc_macro::TokenStream::from(tokens)
 }
--- main.rs
+++ main.rs
@@ -6,4 +6,12 @@
 // To run the code:
 //     $ cargo run
 
+use derive_debug::CustomDebug;
+
+#[derive(CustomDebug)]
+pub struct Field {
+    name: &'static str,
+    bitmask: u8,
+}
+
 fn main() {}
