ea6f59f753d079fd088e4623a6011a9ad5eecc98
Peter Williams <pwil3058@gmail.com>
2019-10-30 08:35:46 +1000

    Pass debug_02 test
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -8,12 +8,34 @@ use syn;
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let parsed_input: syn::DeriveInput = syn::parse_macro_input!(input);
-    let _input_name = parsed_input.ident;
+    let input_name = parsed_input.ident;
+    let mut field_names = vec![];
+    let mut format_str: String = input_name.to_string() + " {{";
+    match parsed_input.data {
+        syn::Data::Struct(s) => {
+            if let syn::Fields::Named(fields) = s.fields {
+                for (i, field) in fields.named.iter().enumerate() {
+                    let field_name = field.ident.as_ref().unwrap();
+                    if i == 0 {
+                        format_str += &format!(" {}: {{:?}}", field_name.to_string());
+                    } else {
+                        format_str += &format!(", {}: {{:?}}", field_name.to_string());
+                    }
+                    let token = quote! {
+                        , self.#field_name
+                    };
+                    field_names.push(token);
+                }
+            }
+        }
+        _ => panic!("not yet implemented"),
+    }
+    format_str += " }}";
 
     let tokens = quote! {
-        impl std::fmt::Debug for Whatever {
+        impl std::fmt::Debug for #input_name {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                write!(f, "Whatever{{}}")
+                write!(f, #format_str #(#field_names)*)
             }
         }
     };
