cecf9a462dfc180b58e9bfdb701a02e3bbf74bb1
Peter Williams <pwil3058@bigpond.net.au>
2021-06-18 09:12:45 +1000

    builder: pass 03-call-setter test

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,57 @@
 use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
+use quote::quote;
+use syn::parse_macro_input;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as syn::DeriveInput);
 
-    unimplemented!()
+    //eprintln!("input: {:#?}", ast);
+
+    let struct_name = &ast.ident;
+    let builder_name = Ident::new(&format!("{}Builder", struct_name), Span::call_site());
+
+    let fields = match &ast.data {
+        syn::Data::Struct(syn::DataStruct {
+            fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+            ..
+        }) => named,
+        _ => panic!("\"#[derive(Builder)]\" only implemented for structs with named fields"),
+    };
+
+    let mut fn_tokens = vec![];
+    for field in fields.iter() {
+        //eprintln!("Fields: {:#?}", field);
+        let field_name = field.ident.as_ref().unwrap();
+        let field_type = &field.ty;
+        let token = quote! {
+            pub fn #field_name(&mut self, #field_name: #field_type) -> &mut Self {
+                let _ = #field_name;
+                self
+            }
+        };
+        fn_tokens.push(token);
+        // match field.ty {
+        //     syn::Type::Path(syn::TypePath { ref path, .. }) => (),
+        //     _ => panic!("\"#[derive(Builder)]\" only implemented for structs with named fields"),
+        // }
+    }
+
+    let tokens = quote!(
+        pub struct #builder_name {
+        }
+
+        impl #builder_name {
+            #(#fn_tokens)*
+        }
+
+        impl #struct_name {
+            pub fn builder() -> #builder_name {
+                #builder_name {}
+            }
+        }
+    );
+
+    TokenStream::from(tokens)
 }
--- main.rs
+++ main.rs
@@ -6,4 +6,20 @@
 // To run the code:
 //     $ cargo run
 
-fn main() {}
+use derive_builder::Builder;
+
+#[derive(Builder)]
+pub struct Command {
+    executable: String,
+    args: Vec<String>,
+    env: Vec<String>,
+    current_dir: String,
+}
+
+fn main() {
+    let mut builder = Command::builder();
+    builder.executable("cargo".to_owned());
+    builder.args(vec!["build".to_owned(), "--release".to_owned()]);
+    builder.env(vec![]);
+    builder.current_dir("..".to_owned());
+}
