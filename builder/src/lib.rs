7b90dcff6e75045a7ac872504e334d54d929db40
Peter Williams <pwil3058@bigpond.net.au>
2021-06-18 10:14:59 +1000

    builder: pass 04-call-build test

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -20,18 +20,34 @@ pub fn derive(input: TokenStream) -> TokenStream {
         _ => panic!("\"#[derive(Builder)]\" only implemented for structs with named fields"),
     };
 
+    let mut field_name_tokens = vec![];
+    let mut declr_tokens = vec![];
     let mut fn_tokens = vec![];
+    let mut build_tokens = vec![];
     for field in fields.iter() {
         //eprintln!("Fields: {:#?}", field);
         let field_name = field.ident.as_ref().unwrap();
+        field_name_tokens.push(quote! {#field_name,});
         let field_type = &field.ty;
         let token = quote! {
+            #field_name: std::option::Option<#field_type>,
+        };
+        declr_tokens.push(token);
+        let token = quote! {
             pub fn #field_name(&mut self, #field_name: #field_type) -> &mut Self {
-                let _ = #field_name;
+                self.#field_name = Some(#field_name);
                 self
             }
         };
         fn_tokens.push(token);
+        let msg = format!("'{}' field has not been set", stringify!(#field_name));
+        let token = quote! {
+            let #field_name = match self.#field_name {
+                Some(ref #field_name) => #field_name.clone(),
+                None => return Err(#msg.to_string().into()),
+            };
+        };
+        build_tokens.push(token);
         // match field.ty {
         //     syn::Type::Path(syn::TypePath { ref path, .. }) => (),
         //     _ => panic!("\"#[derive(Builder)]\" only implemented for structs with named fields"),
@@ -39,16 +55,24 @@ pub fn derive(input: TokenStream) -> TokenStream {
     }
 
     let tokens = quote!(
+        #[derive(Default)]
         pub struct #builder_name {
+            #(#declr_tokens)*
         }
 
         impl #builder_name {
             #(#fn_tokens)*
+
+            pub fn build(&self) -> Result<#struct_name, Box<dyn std::error::Error>> {
+                #(#build_tokens)*
+
+                Ok(#struct_name{#(#field_name_tokens)*})
+            }
         }
 
         impl #struct_name {
             pub fn builder() -> #builder_name {
-                #builder_name {}
+                #builder_name::default()
             }
         }
     );
