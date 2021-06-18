0c7f4b193a7ce406d34d30959b36d1efb580b038
Peter Williams <pwil3058@bigpond.net.au>
2021-06-18 14:10:32 +1000

    builder: pass 06-optional-field test

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -7,8 +7,6 @@ use syn::parse_macro_input;
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as syn::DeriveInput);
 
-    //eprintln!("input: {:#?}", ast);
-
     let struct_name = &ast.ident;
     let builder_name = Ident::new(&format!("{}Builder", struct_name), Span::call_site());
 
@@ -24,34 +22,55 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let mut declr_tokens = vec![];
     let mut fn_tokens = vec![];
     let mut build_tokens = vec![];
+    let ident_option = Ident::new("Option", Span::call_site());
     for field in fields.iter() {
-        //eprintln!("Fields: {:#?}", field);
         let field_name = field.ident.as_ref().unwrap();
-        field_name_tokens.push(quote! {#field_name,});
-        let field_type = &field.ty;
-        let token = quote! {
-            #field_name: std::option::Option<#field_type>,
-        };
-        declr_tokens.push(token);
-        let token = quote! {
-            pub fn #field_name(&mut self, #field_name: #field_type) -> &mut Self {
-                self.#field_name = Some(#field_name);
-                self
-            }
-        };
-        fn_tokens.push(token);
         let msg = format!("'{}' field has not been set", stringify!(#field_name));
-        let token = quote! {
-            let #field_name = match self.#field_name {
-                Some(ref #field_name) => #field_name.clone(),
-                None => return Err(#msg.to_string().into()),
-            };
+        field_name_tokens.push(quote! {#field_name,});
+        match field.ty {
+            syn::Type::Path(syn::TypePath { ref path, .. }) => {
+                let segment = path.segments.first().unwrap();
+                if segment.ident == ident_option {
+                    let option_arg = match segment.arguments {
+                        syn::PathArguments::AngleBracketed(ref args) => &args.args,
+                        _ => panic!("expected angle brackets"),
+                    };
+                    declr_tokens.push(quote! {
+                        #field_name: std::option::Option<#option_arg>,
+                    });
+                    fn_tokens.push(quote! {
+                        pub fn #field_name(&mut self, #field_name: #option_arg) -> &mut Self {
+                            self.#field_name = Some(#field_name);
+                            self
+                        }
+                    });
+                    build_tokens.push(quote! {
+                        let #field_name = match self.#field_name {
+                            Some(ref #field_name) => Some(#field_name.clone()),
+                            None => None,
+                        };
+                    });
+                } else {
+                    let field_type = &field.ty;
+                    declr_tokens.push(quote! {
+                        #field_name: std::option::Option<#field_type>,
+                    });
+                    fn_tokens.push(quote! {
+                        pub fn #field_name(&mut self, #field_name: #field_type) -> &mut Self {
+                            self.#field_name = Some(#field_name);
+                            self
+                        }
+                    });
+                    build_tokens.push(quote! {
+                        let #field_name = match self.#field_name {
+                            Some(ref #field_name) => #field_name.clone(),
+                            None => return Err(#msg.to_string().into()),
+                        };
+                    });
+                }
+            }
+            _ => panic!("expected TypePath"),
         };
-        build_tokens.push(token);
-        // match field.ty {
-        //     syn::Type::Path(syn::TypePath { ref path, .. }) => (),
-        //     _ => panic!("\"#[derive(Builder)]\" only implemented for structs with named fields"),
-        // }
     }
 
     let tokens = quote!(
