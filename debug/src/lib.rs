911f44f12d270a51d071f8fced479714415b8461
Peter Williams <pwil3058@gmail.com>
2021-06-19 21:38:32 +1000

    debug: pass 02-impl-debug test

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,11 +1,37 @@
 use proc_macro::TokenStream;
+use quote::quote;
 use syn::parse_macro_input;
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as syn::DeriveInput);
 
-    eprintln!("INPUT: {:#?}", ast);
+    let struct_name = &ast.ident;
 
-    TokenStream::new()
+    let fields = match &ast.data {
+        syn::Data::Struct(syn::DataStruct {
+            fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+            ..
+        }) => named,
+        _ => panic!("\"#[derive(Builder)]\" only implemented for structs with named fields"),
+    };
+
+    let field_tokens = fields.iter().map(|field| {
+        let field_name = field.ident.as_ref().unwrap();
+        quote! {
+            .field(stringify!(#field_name), &self.#field_name)
+        }
+    });
+
+    let tokens = quote! {
+        impl std::fmt::Debug for #struct_name {
+            fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                fmt.debug_struct(stringify!(#struct_name))
+                    #(#field_tokens)*
+                    .finish()
+            }
+        }
+    };
+
+    tokens.into()
 }
