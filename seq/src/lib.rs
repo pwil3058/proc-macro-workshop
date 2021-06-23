25eafdc546c57537fd8a8797af5ba3aa5b7b70a1
Peter Williams <pwil3058@bigpond.net.au>
2021-06-23 15:20:38 +1000

    seq: pass 01-parse-body test

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -3,8 +3,9 @@ use syn;
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _seq_data = syn::parse_macro_input!(input as SeqData);
+    let seq_data = syn::parse_macro_input!(input as SeqData);
 
+    eprintln!("INNER STREAM: {:#?}", seq_data);
     TokenStream::new()
 }
 
@@ -12,17 +13,21 @@ pub fn seq(input: TokenStream) -> TokenStream {
 struct SeqData {
     ident: syn::Ident,
     range: syn::ExprRange,
-    block: syn::Block,
+    token_stream: TokenStream,
 }
 
 impl syn::parse::Parse for SeqData {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let ident = input.parse::<syn::Ident>()?;
         let _in = input.parse::<syn::Token![in]>()?;
+        let range = input.parse::<syn::ExprRange>()?;
+        let content;
+        let _brace_token = syn::braced!(content in input);
+        let token_stream = proc_macro2::TokenStream::parse(&content)?.into();
         Ok(Self {
             ident,
-            range: input.parse::<syn::ExprRange>()?,
-            block: input.parse::<syn::Block>()?,
+            range,
+            token_stream,
         })
     }
 }
